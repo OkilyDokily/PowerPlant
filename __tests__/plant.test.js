bf7f6145cbfe7575eb8862b4433c4e6809e42f7a
me <me@me.me.com>
2021-03-01 15:59:56 -0800

    nearly finish project

--- __tests__/plant.test.js
+++ __tests__/plant.test.js
@@ -1,14 +1,33 @@
-import { stateControl as plant1, blueFood, superWater } from './../src/plant.js';
+import { storeState, blueFood, superWater, CreatePlantType1, CreatePlantType2,initializedWater, addSpecifiedValueToState,changeStateForPropWithFunction,hydrate} from './../src/plant.js';
 
-describe('stateControl', () => {
+describe('tests', () => {
+  test('should return a function that can modifiy an object according to specified prop', () => {
+    let func = changeStateForPropWithFunction("thunder", x => x + 1);
+    let obj = {};
+    expect(func(obj)["thunder"]).toEqual(1);
+  });
+
+  test('should return correct function and add correctly', () => {
+    expect(addSpecifiedValueToState(7)(5)).toEqual(12);
+  });
+  
   test('should return correct values for new state', () => {
-
+    let plant1 = storeState();
     expect(plant1(blueFood).soil).toEqual(5);
     expect(plant1(superWater).water).toEqual(5);
-    expect(plant1()).toEqual({ "soil": 5, "water": 5 })
-  })
+    expect(plant1()).toEqual({ "soil": 5, "water": 5 });
+  });
 
-  test('should return correct values on closed functions', () => {
+  test('test that initializing state of plant works', () => {
+    let plant2 = storeState();
+    plant2(initializedWater);
+    expect(plant2().water).toEqual(20);
+  });
 
-  })
+  test('test that creating plants of type1 and 2 work', () => {
+    let plant1 = CreatePlantType1();
+    plant1(hydrate);
+    expect(plant1().water).toEqual(21);
+  
+  });
 });
--- src/plant.js
+++ src/plant.js
@@ -1,56 +1,44 @@
-const storeState = (initialState = {}) => {
+export const storeState = (initialState = {}) => {
   let currentState = initialState;
-  return (stateChangeFunction = state => state) => {
+  return (stateChangeFunction = (state => state)) => {
     const newState = stateChangeFunction(currentState);
     currentState = { ...newState };
     return newState;
   };
 };
 
-var plantsArray = [];
-
-
-export function addNewPlant(plantKind) {
-  return () => {
-    const newPlant = storeState();
-    plantsArray.push(newPlant()());
-  };
-}
-
 //function factory
+const initalizeOrResetState = (x) => x ;
+const initializeOrResetStateToValue = (x) => initalizeOrResetState.bind(null,x);
 
-
-const initalizeOrResetState = (x, y) => x = y;
-const initializeOrResetStateToValue = (x) => initalizeOrResetState.apply(x);
-
-const addValueToState = (x, y) => x + y;
+export const addValueToState = (x, y) => x + y;
 
 const subtractValueToState = (x, y) => y - x;
 
-const addSpecifiedValueToState = (x) => addValueToState.apply(x);
-const subtractSpecifiedValueToState = (x) => subtractValueToState.apply(x);
+export const addSpecifiedValueToState = (x) => addValueToState.bind(null,x);
+const subtractSpecifiedValueToState = (x) => subtractValueToState.bind(null,x);
 
 
-
-const changeStateForPropWithFunction = (prop, changeStateFunction) => {
-  return (state) => ({
-    ...state,
-    [prop]: changeStateFunction((state[prop] || 0))()
-  });
+export const changeStateForPropWithFunction = (prop, changeStateFunction) => {
+  return (state) => {
+    let newState = {...state};
+    newState[prop] = changeStateFunction((state[prop] || 0));
+    return newState;
+  };
 };
 
-const initializedShooter = changeStateForPropWithFunction("laser", initializeOrResetStateToValue(100));
+export const initializedShooter = changeStateForPropWithFunction("laser", initializeOrResetStateToValue(100));
+export const initializedSoil = changeStateForPropWithFunction("soil", initializeOrResetStateToValue(50));
+export const initializedWater = changeStateForPropWithFunction("water", initializeOrResetStateToValue(20));
 
-const shootLaser = changeStateForPropWithFunction("laser")(subtractSpecifiedValueToState(5));
-const throwBomb = changeStateForPropWithFunction("bomb")(subtractSpecifiedValueToState(10));
+const shootLaser = changeStateForPropWithFunction("laser",subtractSpecifiedValueToState(5));
+const throwBomb = changeStateForPropWithFunction("bomb",subtractSpecifiedValueToState(10));
 
-const feed = changeStateForPropWithFunction("soil")(addSpecifiedValueToState(1));
-export const blueFood = changeStateForPropWithFunction("soil")(addSpecifiedValueToState(5));
+const feed = changeStateForPropWithFunction("soil",addSpecifiedValueToState(1));
+export const blueFood = changeStateForPropWithFunction("soil",addSpecifiedValueToState(5));
 
-
-const hydrate = changeStateForPropWithFunction("water")(addSpecifiedValueToState(1));
-export const superWater = changeStateForPropWithFunction("water")(addSpecifiedValueToState(5));
-
+export const hydrate = changeStateForPropWithFunction("water",addSpecifiedValueToState(1));
+export const superWater = changeStateForPropWithFunction("water",addSpecifiedValueToState(5));
 
 
 const steal = (property, value) => {
@@ -72,20 +60,25 @@ const SuperStealSoil = steal("soil", 10);
 const StealWater = steal("water", 5);
 const SuperStealWater = steal("water", 10);
 
-export const plant1State = storeState();
-export const plant2State = storeState();
-
-function AddStealWaterAndStealSuperSoil(plant) {
-  return { ...plant(), ...StealWater(), ...SuperStealSoil() };
+export function CreatePlantType1() {
+  
+  var obj = {...StealWater(), ...SuperStealSoil()};
+  let plant = storeState(obj);
+  plant(initializedShooter);
+  plant(initializedWater);
+  plant(initializedSoil);
+  return plant;
 }
 
-function AddSuperStealWaterAndStealSoil(plant) {
-  return { ...plant(), ...SuperStealWater(), ...StealSoil()};
+export function CreatePlantType2() {
+  var obj = { ...SuperStealWater(), ...StealSoil()};
+  let plant = storeState(obj);
+  plant(initializedShooter);
+  plant(initializedWater);
+  plant(initializedSoil);
+  return plant;
 }
 
-function initalizeFirstPlant(plant) {
-  let updatedPlant = AddStealWaterAndStealSuperSoil(plant);
-  plant = storeState()
-}
+
 
 
